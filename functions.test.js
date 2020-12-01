4dab6acbc278e6d0f38cbfd383d4c7317db3b63e
Jessica Michel <jessicamichel93@gmail.com>
2020-12-01 10:57:47 +0100

    adding first tests

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,35 @@
-const functions = require('./functions');
+const { createUser } = require('./functions.js');
+const functions = require('./functions.js')
 
-test('Adds 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+test('checkValue Should be falsy when argument is undefined', () => {
+    expect(functions.checkValue()).toBeUndefined();
+});
+
+test('User should be Brad Traversy object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Brad',
+        lastName: 'Traversy'
+    });
+});
+
+// Less than or greater than
+test('Should be under or equal to 1600', () => {
+    const load1 = 800;
+    const load2 = 800;
+    expect(load1 + load2).toBe(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+    expect('team').not.toBe('I');
+});
+
+// // Arrays
+test('Admin should be in usernames', () => {
+    usernames = ['john', 'karen', 'admin'];
+    expect(usernames).toContain('admin');
 });
